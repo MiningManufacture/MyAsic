1cffd432580d5f65b385547551cbecd6d878e7c3
GliderTeam <baikal-interspace@ya.ru>
2020-05-12 02:52:33 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+#user settings##########################################################################################################
+ip_gateway = '192.168.1.1'
+ProxyServer = "http://117.54.239.18:8080"
+telegram_bot_id = '921805076:ADH3omROOvV8ehtDLcWiZEWAVmwT4-sQ900'
+errors_only = 0
+miner_login = 'root'
+miner_passw = 'root'
+minimal_hashrate = 10000
+interval_sec=3600
+#end of user settings###################################################################################################
