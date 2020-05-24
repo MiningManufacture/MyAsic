23aa34f869918d23cb8212e74dc1e8bc8b693c6b
GliderTeam <baikal-interspace@ya.ru>
2020-05-24 12:43:39 +0800

    Update myasic.py

--- myasic.py
+++ myasic.py
@@ -1,76 +1,92 @@
-################################################ =@MyAsic 2020=#########################################################
-# Monitoring of Bitmain miners S9,S17,L3+################################################################################
-# @MiningManufacture by @Niko_Irk############################################################################################
-import os
+
 import time
 import config
 import subprocess
-import sys
 
-# MAIN###################################################################################################################
-while True:
-    try:
-        mod_inst = subprocess.Popen("py -m pip install --upgrade -q -q -q pip", shell=True, stdout=subprocess.DEVNULL)
+def whatspython():
+    py = ''
+    mod_inst = subprocess.Popen("python --version", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    mod_inst.wait()
+    if mod_inst.returncode == 0:
+        py = 'python'
+        pass
+    else:
+        mod_inst = subprocess.Popen("py --version", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         mod_inst.wait()
         if mod_inst.returncode == 0:
+            py = 'py'
             pass
         else:
-            # print('package: pip3 not found .. try to install')
+            mod_inst = subprocess.Popen("python3 --version", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            mod_inst.wait()
+            if mod_inst.returncode == 0:
+                py = 'python3'
+                pass
+    return py
+
+def getplatform():
+    import platform
+    if platform.system() == 'Windows':
+        return 0
+    else:
+        return 1
+
+def  installwin(module):
+    mod_inst = subprocess.Popen(pypy + " -m pip install --upgrade --no-python-version-warning -q -q -q " + module, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    mod_inst.wait()
+    if mod_inst.returncode == 0:
+        pass
+    return 0
+
+def  installlinux(module):
+    mod_inst = subprocess.Popen(pypy + " -m pip install --upgrade --no-python-version-warning -q -q -q " + module, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    mod_inst.wait()
+    if mod_inst.returncode == 0:
+        pass
+    return 0
+
+pypy = whatspython()
+try:
+    if getplatform() == 0:
+        try:
+            installwin('pip')
+            installwin('wheel')
+            installwin('myasicAPI')
+            installwin('telepot')
+            installwin('requests')
+        except:
+            pass
+
+    else:
+        try:
+            mod_inst = subprocess.Popen("sudo apt-get install --upgrade python3-pip", shell=True,
+                                        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            mod_inst.wait()
+            if mod_inst.returncode == 0:
+                pass
+        except:
             try:
-                mod_inst = subprocess.Popen("sudo apt-get install python3-pip", shell=True, stdout=subprocess.DEVNULL)
+                mod_inst = subprocess.Popen("apt-get install --upgrade python3-pip", shell=True,
+                                            stdout=subprocess.DEVNULL,
+                                            stderr=subprocess.DEVNULL)
                 mod_inst.wait()
                 if mod_inst.returncode == 0:
-                    # print('package: installed')
                     pass
-                else:
-                    print('package: pip installation FAILED! View READMI.md ')
             except:
                 pass
-    except:
-        pass
-
-    try:
-        mod_inst = subprocess.Popen("py -m pip install --upgrade --no-python-version-warning -q -q -q wheel", shell=True, stdout=subprocess.DEVNULL)
-        mod_inst.wait()
-        import wheel
-
-    except:
-        # print('package: myasicAPI not found .. try to install')
         try:
-            mod_inst = subprocess.Popen("pip3 install --upgrade -q -q -q wheel", shell=True, stdout=subprocess.DEVNULL)
-            mod_inst.wait()
-            # print('package: installed')
-        except Exception as e:
-            # print(e)
-            try:
-                import wheel
-            except:
-                print('package: wheel installation FAILED! View READMI.md ')
-                pass
+            installlinux('wheel')
+            installlinux('myasicAPI')
+        except:
+            pass
+except:
+    print('ERROR 1: Cant install some modules!')
+    pass
 
-    try:
-        mod_inst = subprocess.Popen("py -m pip install --upgrade --no-python-version-warning -q -q -q myasicAPI", shell=True, stdout=subprocess.DEVNULL)
-        mod_inst.wait()
-        import myasicAPI
-
-    except:
-        # print('package: myasicAPI not found .. try to install')
-        try:
-            mod_inst = subprocess.Popen("pip3 install --upgrade -q -q -q myasicAPI", shell=True, stdout=subprocess.DEVNULL)
-            mod_inst.wait()
-            import myasicAPI
-            # print('package: installed')
-        except Exception as e:
-            # print(e)
-            try:
-                import myasicAPI
-            except:
-                print('package: myasicAPI installation FAILED! View READMI.md ')
-                pass
-
+# MAIN##################################################################################################################
+while True:
     try:
         import myasicAPI
-        # import myasicAPI_
         myasicAPI.monitoring()
         time.sleep(config.interval_sec)
 
